6154646c8ec9fe6f2ffc5294684b32cfe8beced5
Richárd Németh <nemethricsi@gmail.com>
2019-10-25 19:08:24 +0200

    add Tab component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import './App.css';
+import TabPanel from './components/Tab';
 import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <Navbar />
+    <>
+      <Navbar />
+      <main className="main" >
+        <div className="total-card">Totál</div>
+        <div className="diagram-card">
+          <TabPanel />
+        </div>
+        <div className="transactions-card">
+          Rongyrázások
+        </div>
+      </main>
+    </>
   );
 }
 
---
+++ src/components/Tab/index.js
@@ -0,0 +1,73 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+import PieChartIcon from '@material-ui/icons/PieChart';
+import TimelineIcon from '@material-ui/icons/Timeline';
+
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <Typography
+      component="div"
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      <Box p={3}>{children}</Box>
+    </Typography>
+  );
+}
+
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.any.isRequired,
+  value: PropTypes.any.isRequired,
+};
+
+function a11yProps(index) {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`,
+  };
+}
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.paper,
+  },
+}));
+
+export default function SimpleTabs() {
+  const classes = useStyles();
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Tabs variant="fullWidth" value={value} onChange={handleChange} aria-label="simple tabs example">
+          <Tab icon={<PieChartIcon />} label="Pite" {...a11yProps(0)} />
+          <Tab icon={<TimelineIcon />} label="Csík" {...a11yProps(1)} />
+        </Tabs>
+      </AppBar>
+      <TabPanel value={value} index={0}>
+        Pite Diagram helye
+      </TabPanel>
+      <TabPanel value={value} index={1}>
+        Csík diagram helye
+      </TabPanel>
+    </div>
+  );
+}
