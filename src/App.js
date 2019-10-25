fdb07116fef2ba209228fe14002294b32fbc13af
Richárd Németh <nemethricsi@gmail.com>
2019-10-25 23:54:31 +0200

    add Transactions Header

--- src/App.js
+++ src/App.js
@@ -2,18 +2,31 @@ import React from 'react';
 import './App.css';
 import TabPanel from './components/Tab';
 import Navbar from './components/Navbar';
+import TransactionsHeader from './components/TransactionsHeader';
+import { Paper, Typography } from '@material-ui/core';
+import Kolteslistazo from './components/Kolteslista';
 
 function App() {
   return (
     <>
       <Navbar />
       <main className="main" >
-        <div className="total-card">Totál</div>
+        <div className="total-card">
+          <Paper style={{ padding: '24px 16px', height: '300px' }}>
+            <Typography component="p">
+              Eddigi eltékozolt pénzed:
+              </Typography>
+            <Typography variant="h5" component="h3">
+              20 000 Ft
+            </Typography>
+          </Paper>
+        </div>
         <div className="diagram-card">
           <TabPanel />
         </div>
         <div className="transactions-card">
-          Rongyrázások
+          <TransactionsHeader />
+          <Kolteslistazo />
         </div>
       </main>
     </>
